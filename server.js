e6f92b843c0c951523925be6a2bd07b050617620
Corey Burkett <coreyburkett22@gmail.com>
2020-09-16 12:23:39 -0700

    test

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 const path = require('path');
 
+// test
+
 app.use(express.static(path.join(__dirname, './public/assets')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
